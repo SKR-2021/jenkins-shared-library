1c83b1c9811b83ffb31686702db6ff8158d12de9
karunakar2000 <karunakar2000@gmail.com>
2026-03-19 12:42:02 +0200

    updated

--- vars/EKSDeploy.groovy
+++ vars/EKSDeploy.groovy
@@ -24,25 +24,25 @@ def call (Map configMap){
     //     choice(name: 'deploy_to', choices: ['dev', 'qa', 'prod'], description: 'Pick Something')
     // }
     // This is build section
-    stages {
-
-        stage('Deploy') {
-            steps {
-                script{
-                    withAWS(region:'us-east-1',credentials:'aws-creds') {
-                        sh """
-                            set -e
-                            aws eks update-kubeconfig --region ${REGION} --name ${PROJECT}-${deploy_to}
-                            kubectl get nodes
-                            sed -i "s/IMAGE_VERSION/${env.appVersion}/g" values.yaml
-                            helm upgrade --install ${COMPONENT} -f values-${deploy_to}.yaml -n ${PROJECT} --atomic --wait --timeout=5m . 
-                        """
+        stages {
+            
+            stage('Deploy') {
+                steps {
+                    script{
+                        withAWS(region:'us-east-1',credentials:'aws-creds') {
+                            sh """
+                                set -e
+                                aws eks update-kubeconfig --region ${REGION} --name ${PROJECT}-${deploy_to}
+                                kubectl get nodes
+                                sed -i "s/IMAGE_VERSION/${env.appVersion}/g" values.yaml
+                                helm upgrade --install ${COMPONENT} -f values-${deploy_to}.yaml -n ${PROJECT} --atomic --wait --timeout=5m . 
+                            """
+                        }
                     }
                 }
             }
         }
-    }
-   
+        
         post{
             always{
                 echo 'I will always say Hello again!'
